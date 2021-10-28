e3051caef0b44a7dcc39e1c89d0e3d971bbec6cd
Bradford Gill <51732841+electricdarb@users.noreply.github.com>
2021-10-27 17:21:55 -0700

    Add files via upload

--- mountian_car_Q.py
+++ mountian_car_Q.py
@@ -1,7 +1,8 @@
 import numpy as np
 from math import floor
-import time
 import matplotlib.pyplot as plt
+from celluloid import Camera
+
 
 POSITION_SPACE = (-1.2, .6)
 VELO_SPACE = (-.07, .07)
@@ -13,7 +14,7 @@ it provides a really clean and nice abstraction in this senerio and actually kee
 is import to use tools when appropriate, i think oop is here"""
 
 class Q_table():
-    def __init__(self, discrete_ps = 50, discrete_vs = 50, discrete_as = 3):
+    def __init__(self, discrete_ps = 30, discrete_vs = 30, discrete_as = 3):
         self.p_len, self.v_len, self.a_len = discrete_ps, discrete_vs, discrete_as
         self.map_p_to_i = lambda p: floor((p - POSITION_SPACE[0])  / (POSITION_SPACE[1] - POSITION_SPACE[0]) * (discrete_ps - 1))
         self.map_v_to_i = lambda v: floor((v - VELO_SPACE[0])  / (VELO_SPACE[1] - VELO_SPACE[0]) * (discrete_vs - 1))
@@ -54,23 +55,26 @@ class Q_table():
         # bellman eq: Qnew = Q + lr(reward + discount * best_action_est, - Q) 
         self.table[i, j, k] = Q + lr * (reward + discount * np.max(self.table[i_prime, j_prime, :]) - Q) 
 
-def simulate(discount, lr, e_decay_rate, max_iters = 6000):
-    reward = np.zeros(max_iters) 
+def simulate(discount, lr, e_decay_rate, max_iters = 5000, return_table = False):
+    rewards = np.zeros(max_iters) 
     Qt = Q_table()
     epsilon = 1 # prob of exploring, 1- epsilon = prob of exploiting (best)
     e_decay = lambda e: max(e * e_decay_rate, .01) # how fast epsilon decays, the max comes from deep minds paper
     for i in range(max_iters):
         p, v = np.random.uniform(*START_RANGE), 0
-        steps = 0
-        while p < POSITION_SPACE[1] and steps < 250: # stop infintite loop
+        reward = 0
+        while p < POSITION_SPACE[1] and reward < 250: # stop infintite loop
             a_fn = np.random.choice([Qt.get_best_action, Qt.get_random_action], p = [1 - epsilon, epsilon])
             a = a_fn(p, v)
             Qt.update_Q(-1, lr, discount, p, v, a)
             p, v = Qt.update_state(p, v, a)
-            steps += 1
-        reward[i] = steps
+            reward += 1
+        rewards[i] = reward
         epsilon = e_decay(epsilon)
-    return reward
+    if return_table:
+        return rewards, Qt
+    else:
+        return rewards
 
 def hyper_search():
     """
@@ -78,7 +82,7 @@ def hyper_search():
     there iterate. 
     """
     trials = 20
-    scores = np.zeros(trials)
+    scores = []
     discount_range = (.1, 1.)
     lr_range = (.1, .4)
     e_decay_range = (.5, 1.)
@@ -88,17 +92,59 @@ def hyper_search():
     hp['epsilon_decay'] = np.random.uniform(*e_decay_range, trials)
 
     for i in range(trials):
-        score = simulate(hp['discount'][i], hp['lr'][i], hp['epsilon_decay'][i])
-        scores[i] = min(score)
+        hold = (hp['discount'][i], hp['lr'][i], hp['epsilon_decay'][i])
+        score = simulate(*hold)
+        scores.append(score)
+        print("score: {}, discount: {}, lr: {}, epsilon_decay: {}".format(np.average(score[-500:]), *hold))
 
-    fix, axs = plt.subplots(3, 1)
+    fig, axs = plt.subplots(3, 1)
     for ax, key in zip(axs, hp.keys()):
-        ax.scatter(hp[key], scores)
+        ax.scatter(hp[key], np.average(scores[i]))
         ax.set_title(key)
-        ax.set_ylim([0, 1000])
-    plt.show()
+    fig.show()
+    """
+    score: 211.72, discount: 0.2824933520659566, lr: 0.17244939592088193, epsilon_decay: 0.9422534370060514
+    score: 221.968, discount: 0.449881569041625, lr: 0.2797153971134617, epsilon_decay: 0.5612766255280709
+    score: 212.474, discount: 0.47365344896391615, lr: 0.34464527505672415, epsilon_decay: 0.7545763229312964
+    score: 147.08, discount: 0.8941097749008459, lr: 0.281720165576596, epsilon_decay: 0.6402003793646793
+    score: 191.864, discount: 0.694823346681323, lr: 0.1975957805539046, epsilon_decay: 0.6081500458326561
+    score: 223.624, discount: 0.5669016355520428, lr: 0.3532305508549993, epsilon_decay: 0.6683549632717782
+    score: 193.684, discount: 0.9403139125426704, lr: 0.38720828795289886, epsilon_decay: 0.9810833438406337
+    score: 175.858, discount: 0.4590723333145095, lr: 0.22499849644943165, epsilon_decay: 0.5712505297039662
+    score: 183.182, discount: 0.4603140408915277, lr: 0.1298694973714855, epsilon_decay: 0.7293091177345252
+    score: 250.0, discount: 0.15668291093964357, lr: 0.37399716877833344, epsilon_decay: 0.6667601971835406
+    score: 189.818, discount: 0.9302288566529271, lr: 0.3461452836964455, epsilon_decay: 0.5225729267470409
+    score: 206.89, discount: 0.7393388279997576, lr: 0.38404711840235606, epsilon_decay: 0.6978357947772437
+    score: 250.0, discount: 0.13798033479312335, lr: 0.3941747862632219, epsilon_decay: 0.9399145331098893
+    score: 198.196, discount: 0.7249406276042496, lr: 0.233962415206557, epsilon_decay: 0.65366939065216
+    score: 222.288, discount: 0.18603651792662607, lr: 0.10025206638754855, epsilon_decay: 0.746878295600906
+    score: 214.428, discount: 0.2933310640544758, lr: 0.11295598407531794, epsilon_decay: 0.9064368432918122
+    score: 179.178, discount: 0.6077255751840971, lr: 0.1592095997311603, epsilon_decay: 0.7834561753174173
+    score: 172.55, discount: 0.8588934763634241, lr: 0.11672460536562612, epsilon_decay: 0.700945100569841
+    score: 231.534, discount: 0.6922898626050366, lr: 0.25278751556240864, epsilon_decay: 0.8105051350601509
+    score: 248.862, discount: 0.1599106876760406, lr: 0.13941395688375366, epsilon_decay: 0.7350357615629758
+    """
+
+def train_and_plot(lr, discount, epsilon_decay):
+    score, Qt = simulate(lr, discount, epsilon_decay, return_table = True)
+    N = 400 # thanks bill leonard
+    x = np.linspace(*POSITION_SPACE, N)
+    fn = lambda x: np.sin(3*x)
+    y = fn(x)
+
+    fig, ax = plt.subplots(1, 1)
+    camera = Camera(fig)
+    
+    p, v = -.5, 0.
+    while p < POSITION_SPACE[1]: # stop infintite loop
+        a = Qt.get_best_action(p, v)
+        p, v = Qt.update_state(p, v, a)
+        ax.scatter(p, fn(p), s = 15, c = 'r')
+        ax.plot(x, y, c = 'b') # i know repeative but im not memory constrained so im not going to rewrite
+        camera.snap()
+    animation = camera.animate(fps = 30))
+    animation.save('mountiancar.gif', writer='Pillow')
 
 if __name__ == "__main__":
-    hyper_search()
+    train_and_plot(.894, .282, .640)
     
-
